6a33bf847b4e44061b88c65be496b732145169ac
Jaydeep2907 <106866364+Jaydeep2907@users.noreply.github.com>
2022-07-02 15:12:57 +0530

    changes
    changes

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 @section('content')
 <div class="row">
     <div class="col-lg-12 margin-tb">
@@ -19,41 +19,39 @@
 </div>
 @endif
 
-<table class="table table-bordered table-responsive-lg">
-    <tr>
-        <th>No</th>
-        <th>Name</th>
-        <th>description</th>
-        <th>Price</th>
-        <th>Date Created</th>
-        <th>Actions</th>
-    </tr>
-    @foreach ($products as $product)
-    <tr>
-        <td>{{ $product->id }}</td>
-        <td>{{ $product->name }}</td>
-        <td>{{ $product->description }}</td>
-        <td>{{ $product->price }}</td>
-        <td>{{ $product->created_at }}</td>
-        <td>
-            <form action="{{ route('products.destroy',$product->id) }}" method="POST">
-<!--                <a href="" title="show">
-                    <i class="fas fa-eye text-success  fa-lg"></i>
-                </a>-->
-                <a href="{{ route('products.edit',$product->id) }}">
-                    <i class="fas fa-edit fa-lg"></i>
-                </a>
-                @csrf
-                @method('DELETE')
-                <button type="submit" title="delete" style="border: none; background-color:transparent;">
-                    <i class="fas fa-trash fa-lg text-danger"></i>
-                </button>
-            </form>
-        </td>
-    </tr>
-    @endforeach
+<table id="example" class="table is-striped" style="width:100%">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>description</th>
+            <th>Price</th>
+            <th>Date Created</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ $product->id }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->description }}</td>
+            <td>{{ $product->price }}</td>
+            <td>{{ $product->created_at }}</td>
+            <td>
+                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+                    <a href="{{ route('products.edit',$product->id) }}"><i class="fas fa-edit fa-lg"></i></a>
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" title="delete" style="border: none; background-color:transparent;">
+                        <i class="fas fa-trash fa-lg text-danger"></i>
+                    </button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
 </table>
-
-{!! $products->links() !!}
-
 @endsection
+
+
